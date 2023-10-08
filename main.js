7b330f300a196a71f7ece22de6c80f47a62a131b
Merojxon <sirojiddinovmerojiddin66@gmail.com>
2023-10-08 21:12:29 +0500

    all issues are resolved

---
+++ main.js
@@ -0,0 +1,444 @@
+// let borishKelish$ = 500;
+// let mehmonxona$ = 250;
+// let muzeyEuro = 120;
+// const $ = 11000.34;
+// const euro = 12354.03;
+
+// const myMoney = prompt('Pulingizni kiriting: ');
+
+// let expenses = borishKelish$*$ + mehmonxona$*$ +muzeyEuro*euro;
+
+// if(parseInt(myMoney) > expenses){
+//     console.log("Oq yo'l")
+// }else{
+//     console.log("Ozgina sabr qilish kerak");
+// }
+
+
+
+
+
+// // If opetaroti - 1
+
+// let intNumber = parseInt(prompt("Butun son kiriting: "))
+
+// if(intNumber > 0){
+//     alert(intNumber + 1);
+// }else{
+//     alert(intNumber);
+// }
+
+
+
+
+// // if operatori -2
+// let intNumber = parseInt(prompt("Butun son kiriting: "))
+
+// if(intNumber > 0){
+//     alert(intNumber + 1);
+// }else{
+//     alert(intNumber - 2);
+// }
+
+
+
+
+// // if operatori -3
+// let intNumber = parseInt(prompt("Butun son kiriting: "))
+
+// if(intNumber > 0){
+//     alert(intNumber + 1);
+// }else if(intNumber = 0){
+//     intNumber = 10;
+//     alert(intNumber);
+// }else{
+//     alert(intNumber - 2);
+// }
+
+
+
+
+// // // if operatori -4
+// let intNumber1 = prompt("Butun son kiriting: ")
+// let intNumber2= prompt("Butun son kiriting: ")
+// let intNumber3 = prompt("Butun son kiriting: ")
+
+// let positiveNumber = 0;
+
+// if(intNumber1 > 0){
+//     positiveNumber++
+// }if(intNumber2 > 0){
+//     positiveNumber++
+// }if(intNumber3 > 0){
+//     positiveNumber++
+// }
+
+// alert(positiveNumber)
+
+
+
+
+
+// // // if operatori -5
+// let intNumber1 = prompt("Butun son kiriting: ")
+// let intNumber2= prompt("Butun son kiriting: ")
+// let intNumber3 = prompt("Butun son kiriting: ")
+
+// let positiveNumber = 0;
+// let negativeNumber = 0;
+
+// if(intNumber1 > 0){
+//     positiveNumber++
+// }else{
+//     negativeNumber++
+// }
+// if(intNumber2 > 0){
+//     positiveNumber++
+// }else{
+//     negativeNumber++
+// }
+// if(intNumber3 > 0){
+//     positiveNumber++
+// }else{
+//     negativeNumber++
+// }
+
+// alert(`Musbat sonlar ${positiveNumber}ta, manfiy sonlar ${negativeNumber}ta`)
+
+
+
+// // If-6
+// let intNumber1 = prompt("Butun son kiriting: ")
+// let intNumber2= prompt("Butun son kiriting: ")
+
+// if(intNumber1>intNumber2){
+//     alert(`kiritilgan sonlar rasidan eng kattasi ${intNumber1}`);
+// }else{
+//     alert(`kiritilgan sonlar rasidan eng kattasi ${intNumber2}`);
+
+// }
+
+
+
+// // If-7
+// let intNumber1 = prompt("Butun son kiriting: ")
+// let intNumber2= prompt("Butun son kiriting: ")
+
+// if(intNumber1>intNumber2){
+//     alert(`1`);
+// }else{
+//     alert(`2`);
+// }
+
+
+
+// // If-8
+// let intNumber1 = prompt("Butun son kiriting: ")
+// let intNumber2= prompt("Butun son kiriting: ")
+
+// if(intNumber1>intNumber2){
+//     alert(`${intNumber1}, ${intNumber2}`);
+// }else{
+//     alert(`${intNumber2}, ${intNumber1}`);
+
+// }
+
+
+// // If-9
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// if(a > b) {
+//     alert(a-b)
+//     alert(b+a)
+// }else {
+//     alert(a)
+//     alert(b)
+// }
+
+
+
+// If-10
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// if(a===b){
+//     a = a+b;
+//     b = a+b
+// }else{
+//     a=0
+//     b=0
+// }
+// alert(`${a}, ${b}`)
+
+
+
+
+// // If-11
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// if(a>b){
+//     a = a;
+//     b = a
+// }else if(b>a){
+//     a=b
+//     b=b
+// }else{
+//     a=0
+//     b=0
+// }
+// alert(`${a}, ${b}`)
+
+
+
+
+// // If-12
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// if(a < b && a < c ) {
+//     alert(a)
+// }else if(b<a && b < c) {
+//     alert(b)
+// }else {
+//     alert(c)
+// }
+
+
+
+// // If-13
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// if(a>b>c && c>b>a){
+//     alert(b);
+// }else if(a>c>b && b>c>a){
+//     alert(c);
+// }else{
+//     alert(a);
+// }
+
+
+
+// // If-14
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// if(a>b>c){
+//     alert(a);
+//     alert(c);
+// }else if(a>c>b){
+//     alert(a);
+//     alert(b);
+// }else if(b>c>a){
+//     alert(b);
+//     alert(a);
+// }else if(b>a>c){
+//     alert(b);
+//     alert(c);
+// }else if(c>b>a){
+//     alert(c);
+//     alert(a);
+// }else{
+//     alert(c);
+//     alert(b);
+// }
+
+
+
+// // If-15
+
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// if(a>b && b>c || b>a && a>c){
+//     alert(a+b);
+// }else if(a>c && c>b || c>a && a>b){
+//     alert(a+c);
+// }else{
+//     alert(b+c);
+// }
+
+
+
+// // boolean-1
+// let a=prompt('son kiriting: ')
+
+// alert(a>0);
+
+
+// // Boolean-2
+// let a=prompt('son kiriting: ')
+
+// alert(a%2!=0)
+
+
+// // Boolean-3
+// let a=prompt('son kiriting: ')
+
+// alert(a%2==0)
+
+// // Boolean-4
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// alert(a>2 && b<=3)
+
+
+
+// // Boolean-5
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// alert(a>=0 && b<-2)
+
+
+
+// // Boolean-6
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// alert(a<=b<=c)
+
+
+// Boolean-7
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// alert(a>b>c)
+
+
+// // Boolean-8
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// alert(a%2!=0 && b%2!=0)
+
+
+
+// Boolean-9
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// alert(a%2!=0 || b%2!=0)
+
+
+// // Boolean-10
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// alert(!((a%2!=0 && b%2!=0) || (a%2==0 && b%2==0)) && (a%2!=0 || b%2!=0))
+
+
+
+// // Boolean-11
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+
+// alert((a%2!=0 && b%2!=0) || (a%2==0 && b%2==0))
+
+
+
+
+// // Boolean-12
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// alert(a>0&&b>0&&c>0)
+
+
+
+// // Boolean-13
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// alert(a>0||b>0||c>0)
+
+
+// // Boolean-14
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// alert(!((a>0 && b>0 && c>0) || (a<0 && b<0 && c<0) || ((a<0 && b<0)||(a<0 && c<0)||(b<0&&c<0))) && (a>0 || b>0 || c>0))
+
+
+
+// //Boolean-15
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+// alert(!((a>0 && b>0 && c>0) || (a<0 && b<0 && c<0) && ((a<0 && b<0)||(a<0 && c<0)||(b<0&&c<0))) && (a>0 || b>0 || c>0))
+
+
+
+
+// // Boolean-16
+// let a = prompt('Butun son kiriting')
+
+// alert(a%2==0 && a/100<1)
+
+
+// // Boolean-17
+// let a = prompt('Butun son kiriting')
+
+// alert(a%2!=0 && a/100>1 && a/1000<1)
+
+
+// //Boolean-18
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+
+// alert(a==b || a==c || b==c)
+
+
+
+
+// //Boolean-19
+// let a = prompt("Butun son kiriting: ")
+// let b= prompt("Butun son kiriting: ")
+// let c= prompt("Butun son kiriting: ")
+
+
+// alert(a==-b || a==-c || b==-c)
+
+
+
+// // Boolean-20
+// let a = prompt("Butun 3xonali son kiriting: ")
+
+// alert((a%10!=(a%1000-a%100)/100) && (a%10!=(a%100-a%10)/10) && ((a%100-a%10)/10!=(a%1000-a%10)/100))
+
+
+
+
+// // Boolean-21
+// let a = prompt("Butun 3xonali son kiriting: ")
+
+// alert((a%10==(a%100-a%10)/10+1)&&(a%100-a%10)/10==(a%1000-a%100)/100+1)
+
+
+
+// // Boolean-22
+// let a = prompt("Butun 3xonali son kiriting: ")
+
+// alert(((a%10==(a%100-a%10)/10+1)&&(a%100-a%10)/10==(a%1000-a%100)/100+1)||(a%10==(a%100-a%10)/10-1)&&(a%100-a%10)/10==(a%1000-a%100)/100-1)
+
+
+
+// // Boolean-23
+// let a = prompt("Butun 3xonali son kiriting: ")
+
+// alert(a%10==(a%1000-a%100)/100)
